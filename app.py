ef35d5cb5ee4d2802064996849300e265a50a9ef
Reyner Veliz <reynerv@yahoo.com>
2023-07-10 07:25:49 -0600

    about

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 print(__name__)
 if __name__ == '__main__':
