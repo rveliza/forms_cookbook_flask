b7c2634ac2cb4a408a470a959c952ad1b3e6e343
Reyner Veliz <reynerv@yahoo.com>
2023-07-10 13:01:30 -0600

    create forms.py: add CommentForm, app.py: initialize CommentForm and render with template_form, recipe.html: create form, app.py: store data in new_comment and append to comments

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from helper import recipes, types, descriptions, ingredients, instructions, comments
+from forms import CommentForm
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "mysecret"
 
 @app.route('/')
 def index():
@@ -10,18 +12,28 @@ def index():
     return render_template("index.html",
                            template_recipes = recipes)
 
-@app.route('/recipe/<int:id>')
+@app.route('/recipe/<int:id>', methods=["GET", "POST"])
 def recipe(id):
     # print(recipes[id])
     # "buttered toast"
     # return "Hello" + str(id)
+    comment_form = CommentForm(csrf_enabled=False)
+    # print(comment_form)
+    # <forms.CommentForm object at 0x000001D0C7E3FFA0>
+    # print(comment_form.comment)
+    # <input id="comment" name="comment" type="text" value="">
+    # print(comment_form.submit)
+    # <input id="submit" name="submit" type="submit" value="Add Comment">
+    new_comment = comment_form.comment.data
+    comments[id].append(new_comment)
     return render_template("recipe.html",
                            template_recipes = recipes[id],
                            template_type = types[id],
                            template_description = descriptions[id],
                            template_ingredients = ingredients[id],
                            template_instructions = instructions[id],
-                           template_comments = comments[id])
+                           template_comments = comments[id],
+                           template_form = comment_form)
 
 @app.route('/about')
 def about():
